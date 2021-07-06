42b99caad2efb1e2a0350bfbe871896db6cb99fe
1273611319@qq.com <1273611319@qq.com>
2021-07-06 21:38:12 +0800

    给项目添加图标

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { GlobalStyle } from './style';
+import { IconStyle } from './assets/iconfont/iconfont';
 
 function App() {
   return (
     <div>
       <h1>Cloud-Music</h1>
+      <GlobalStyle />
+      <IconStyle />
+      <i className="iconfont">&#xe62b;</i>
     </div>
   );
 }
