dd8666604e4ff74b999ff91f1c46abd6dd9a7f79
谭静（Tyrone） <Tyrone.tan@aishu.cn>
2021-07-08 16:12:20 +0800

    添加Slider轮播公共组件

--- src/application/Recommend/index.jsx
+++ src/application/Recommend/index.jsx
@@ -1,8 +1,15 @@
-import {memo} from 'react';
+import { memo } from "react";
+import Slider from "../../components/Slider";
 
-const Recommend = props => {
-
-    return <div>Recommend</div>
-}
+const Recommend = (props) => {
+  const bannerList = [1, 2, 3, 4].map((item) => {
+    return {
+      id: item,
+      imageUrl:
+        "http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg",
+    };
+  });
+  return <div>{<Slider bannerList={bannerList}></Slider>}</div>;
+};
 
 export default memo(Recommend);
