f8102c88288a3751e9e4da18dceacf9e41e4fd1b
谭静（Tyrone） <Tyrone.tan@aishu.cn>
2021-07-12 17:10:36 +0800

    使用useSelector和useDisapath替换connect

--- src/application/Recommend/index.jsx
+++ src/application/Recommend/index.jsx
@@ -1,27 +1,77 @@
 import { memo, useEffect } from "react";
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import RecommendList from "./RecommendList";
 import Slider from "../../components/Slider";
 import * as actionTypes from "./store/actionCreators";
 import Scroll from "../../baseUI/Scroll";
 import { Content } from "./style";
 
-const Recommend = (props) => {
-  const { bannerList, recommendList } = props;
-  const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
+// const Recommend = (props) => {
+//   const { bannerList, recommendList } = props;
+//   const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
 
-  useEffect(() => {
-    getBannerDataDispatch();
-    getRecommendListDataDispatch();
-    // eslint-disable-next-line
-  }, []);
+//   useEffect(() => {
+//     getBannerDataDispatch();
+//     getRecommendListDataDispatch();
+//     // eslint-disable-next-line
+//   }, []);
 
+//   const bannerListJS = bannerList ? bannerList.toJS() : [];
+//   const recommendListJS = recommendList ? recommendList.toJS() : [];
+
+//   console.log(bannerListJS, "bannerListJS");
+//   console.log(recommendListJS, "recommendListJS");
+//   console.log("test");
+
+//   return (
+//     <Content>
+//       <Scroll className="list" onScroll={() => console.log("test")}>
+//         <div>
+//           <Slider bannerList={bannerListJS}></Slider>
+//           <RecommendList recommendList={recommendListJS}></RecommendList>
+//         </div>
+//       </Scroll>
+//     </Content>
+//   );
+// };
+
+// // 映射Redux全局的state到组件的props上
+// const mapStateToProps = (state) => ({
+//   // 不要在这里将数据 toJS
+//   // 不然每次 diff 比对 props 的时候都是不一样的引用，还是导致不必要的重渲染，属于滥用 immutable
+//   bannerList: state.getIn(["recommend", "bannerList"]),
+//   recommendList: state.getIn(["recommend", "recommendList"]),
+// });
+
+// // 映射 dispatch 到 props 上
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     getBannerDataDispatch() {
+//       dispatch(actionTypes.getBannerList());
+//     },
+//     getRecommendListDataDispatch() {
+//       dispatch(actionTypes.getRecommendList());
+//     },
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(memo(Recommend));
+
+const Recommend = memo(() => {
+  const bannerList = useSelector((state) => {
+    return state.getIn(["recommend", "bannerList"]);
+  });
+  const recommendList = useSelector((state) =>
+    state.getIn(["recommend", "recommendList"])
+  );
   const bannerListJS = bannerList ? bannerList.toJS() : [];
   const recommendListJS = recommendList ? recommendList.toJS() : [];
-
-  console.log(bannerListJS, "bannerListJS");
-  console.log(recommendListJS, "recommendListJS");
-  console.log("test");
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(actionTypes.getBannerList());
+    dispatch(actionTypes.getRecommendList());
+    // eslint-disable-next-line
+  }, []);
 
   return (
     <Content>
@@ -33,26 +83,5 @@ const Recommend = (props) => {
       </Scroll>
     </Content>
   );
-};
-
-// 映射Redux全局的state到组件的props上
-const mapStateToProps = (state) => ({
-  // 不要在这里将数据 toJS
-  // 不然每次 diff 比对 props 的时候都是不一样的引用，还是导致不必要的重渲染，属于滥用 immutable
-  bannerList: state.getIn(["recommend", "bannerList"]),
-  recommendList: state.getIn(["recommend", "recommendList"]),
 });
-
-// 映射 dispatch 到 props 上
-const mapDispatchToProps = (dispatch) => {
-  return {
-    getBannerDataDispatch() {
-      dispatch(actionTypes.getBannerList());
-    },
-    getRecommendListDataDispatch() {
-      dispatch(actionTypes.getRecommendList());
-    },
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(memo(Recommend));
+export default Recommend;
